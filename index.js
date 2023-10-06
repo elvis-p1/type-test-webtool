bef4d18c184cabba239af7b4e5c63c13665b0cfb
ephung-main <elvisphung.email@gmail.com>
2023-10-05 21:06:46 -0400

    change button events to addEventListeners, add styling for text display

--- index.js
+++ index.js
@@ -4,16 +4,10 @@ class TimerObject {
 		this.endTime;
 		this.charactersTyped = 0;
 		this.started = false;
+		this.timeoutID = undefined;
 	}
-	
-	startTimer() {
-		this.charactersTyped = 0;
-		this.startTime = new Date();
-		document.getElementById("text-area").disabled = false;
-		document.getElementById("end-button").disabled = false;
-	};
-	
-	getElapsedTime() {
+
+	getWordsPerMinute() {
 		this.endTime = new Date();
 		var timeDiff = this.endTime - this.startTime; //in ms
 	
@@ -25,7 +19,35 @@ class TimerObject {
 		var wordsPerMinute = wordsPerSecond*60;
 		console.log(wordsPerMinute.toFixed(2) + " WPM");
 		
+		if(isNaN(wordsPerMinute)) {
+			return 0;
+		}
+
 		return wordsPerMinute;
+	}
+
+	updateWPMLabel() {
+		document.getElementById("speed-text").innerHTML = "WPM: " + timer.getWordsPerMinute().toFixed(2).toString();
+		
+		this.timeoutID = setTimeout(() => {
+			this.updateWPMLabel();
+		}, "2500");
+	}
+
+	startTimer() {
+		this.charactersTyped = 0;
+		this.startTime = new Date();
+		document.getElementById("text-area").disabled = false;
+		document.getElementById("end-button").disabled = false;
+		this.updateWPMLabel();
+	};
+	
+	stopTimer() {
+		document.getElementById("speed-text").innerHTML = "WPM: " + timer.getWordsPerMinute().toFixed(2).toString();
+		clearTimeout(this.timeoutID);
+		document.getElementById("text-area").disabled = true;
+		document.getElementById("end-button").disabled = true;
+		clearTextField();
 	}
 
 	incrementCharactersTyped() {
@@ -33,15 +55,29 @@ class TimerObject {
 	}
 
 	getStarted() {
-		return this.started;
+		return this.started;wordsPerMinute()
 	}
 }
 
 timer = new TimerObject();
 
-function aFunction() {
-	timer.incrementCharactersTyped();
-	document.getElementById("speed-text").innerHTML = "WPM: " + timer.getElapsedTime().toFixed(2).toString();
+function clearTextField() {
+	document.getElementById("text-area").value = ""
 }
 
+const textAreaElement = document.getElementById("text-area");
+textAreaElement.addEventListener("input", function() {
+	timer.incrementCharactersTyped();
+	if(textAreaElement.value.slice(-1) === " ") {
+		clearTextField();
+	}
+}, false);
 
+const startButtonElement = document.getElementById("start-button");
+const endButtonElement = document.getElementById("end-button");
+startButtonElement.addEventListener("click", function() {
+	timer.startTimer();
+}, false);
+endButtonElement.addEventListener("click", function() {
+	timer.stopTimer();
+}, false);
